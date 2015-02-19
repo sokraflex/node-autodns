c2991cdc745b0c1ff6ff6b9bf7517ffc45980bc9
SargTeX <martin.christian.bories@gmail.com>
2015-02-19 14:38:10 +0100

    Added Zone support
    One may now create, update, delete, get and list the zones.
    Although list support isn't fully finished yet.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# node-autodns
+node-autodns
+============
+
 NodeJS client for AutoDNS 3 13.1
+
+
+API
+===
+
+```var AutoDNS = require('autodns');
+
+// 4 => context-id, received by the InternetX administration for the api endpoint
+var autodns = new AutoDNS('Username', 'Password', 4, 'en', 'my-contact@mail-adress.net', 'https://gateway.autodns.com');
+
+autodns.getDomain('mydomain.com', function(err, domain) {
+	if (err) throw err;
+	console.log(domain); // general information about the domain, like adminc, techc, ownerc, ...
+});
+
+autodns.transferDomainIn({name: 'mydomain.com', authinfo: 'faduigr73284zrodjsa89'}, function(err, result) {
+	if (err) throw err;
+	console.log(result);
+});```
--- autodns.js
+++ autodns.js
@@ -1,14 +1,16 @@
 var async = require('async'),
 	https = require('https'),
 	js2xmlparser = require('js2xmlparser'),
+	url = require('url'),
 	xml2js = require('xml2js');
 
-var autodns = function(user, password, context, language, email) {
+var autodns = function(user, password, context, language, email, baseUrl) {
 	this.user = user;
 	this.password = password;
 	this.context = context;
 	this.language = language || 'en';
 	this.email = email;
+	this.baseUrl = baseUrl || 'https://gateway.autodns.com';
 };
 
 /**
@@ -26,12 +28,9 @@ autodns.prototype.getDefaultNameservers = function() {
 /**
  * API Methods.
  */
-autodns.prototype.getDomain = function(defaults, options, callback) {
-	if (!callback) {
-		callback = options;
-		options = defaults;
-		defaults = null;
-	}
+
+/** DOMAINS **/
+autodns.prototype.getDomain = function(options, callback) {
 	if (typeof options === 'string') options = {name: options};
 	if (Array.isArray(options)) return this.getDomains(options, callback);
 
@@ -74,6 +73,55 @@ autodns.prototype.getDomains = function(domains, callback) {
 	});
 };
 
+/** ZONES **/
+autodns.prototype.createZone = function(defaults, data, callback) {
+	if (!callback) {callback = data; data = defaults; defaults = null;}
+
+	var data = this._sign('0201', 'zone', data, defaults);
+	this._request(data, function(err, response) {
+		if (err) return callback(err);
+		callback(false, response.status);
+	})
+};
+
+autodns.prototype.deleteZone = function(data, callback) {
+	if (typeof data === 'string') data = {name: data};
+
+	var data = this._sign('0203', 'zone', data);
+	this._request(data, function(err, response) {
+		if (err) return callback(err);
+		callback(false, response.status);
+	})
+};
+
+autodns.prototype.getZone = function(data, callback) {
+	if (typeof data === 'string') data = {name: data};
+
+	var data = this._sign('0205', 'zone', data);
+	this._request(data, function(err, response) {
+		if (err) return callback(err);
+		callback(false, response.data.zone);
+	})
+};
+
+autodns.prototype.listZones = function(viewOptions, whereOptions, keys, callback) {
+	var data = this._sign({code: '0205', view: viewOptions, where: whereOptions, key: keys});
+	this._request(data, function(err, response) {
+		if (err) return callback(err);
+		callback(false, response.data.zone);
+	})
+}
+
+autodns.prototype.updateZone = function(defaults, data, callback) {
+	if (!callback) {callback = data; data = defaults; defaults = null;}
+
+	var data = this._sign('0202', 'zone', data, defaults);
+	this._request(data, function(err, response) {
+		if (err) return callback(err);
+		callback(false, response.status);
+	})
+};
+
 /**
  * Internal methods
  * Only for our private use, just to make this whole thing work and easy for us to develop
@@ -88,8 +136,9 @@ autodns.prototype._sign = function(code, objectType, data, defaults, options) {
 			code: code
 		}
 	};
+	if (!(typeof code === 'string')) request.task = code;	
 	if (defaults) request.task['default'] = defaults;
-	request.task[objectType] = data;
+	if (objectType) request.task[objectType] = data;
 
 	if (options.replyTo) request.task.reply_to = this.email;
 	return js2xmlparser('request', request);
@@ -114,12 +163,9 @@ autodns.prototype._getAuth = function() {
  	data => response from AutoDNS, formatted as an Object, starting in the <response><result><data><domain_or_contact_or_...>-Tag
  */
 autodns.prototype._request = function(data, callback) {
-	var request = https.request({
-			hostname: 'gateway.autodns.com',
-			port: 443,
-			path: '/',
-			method: 'POST'
-		}, function(res) {
+	var options = url.parse(this.baseUrl, false);
+	options.method = 'POST';
+	var request = https.request(options, function(res) {
 			res.setEncoding('UTF-8');
 			var response = '';
 			res.on('end', function(err) {
